098cac427db7ff3f41f12d63a78520ed7dcb682a
PRASOON KUMAR <prasoonparashar49@gmail.com>
2022-02-07 23:34:39 +0530

    removed commented standard mysql connection establishment code

--- BlueDTS/BlueDTS.SQL/DAL/DB_Demo.js
+++ BlueDTS/BlueDTS.SQL/DAL/DB_Demo.js
@@ -1,24 +1,4 @@
 var mySql = require('mysql');
-/*
-var con = mySql.createConnection({
-    host: 'dev.bluesecures.com',
-    port: 3306,
-    user: 'dev',
-    password: 'Blue@1234',
-    connectTimeout: 30000
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log('Connected!');
-    
-    //con.query('select * from ejabberd.archive where timestamp = 1637681859787947', function(err, result) {
-    //    if(err) throw err;
-    //    console.log("Result: " + result);
-    //});
-    
-});
-*/
 var pool = mySql.createPool({
     connectionLimit : 1,
     host            : 'dev.bluesecures.com',
